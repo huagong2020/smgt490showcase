4b79712421e76f594559e5cc6ba8542fb8e90974
Hua Gong <hg37@rice.edu>
2026-01-12 22:57:55 -0600

    updates

---
+++ projects.js
@@ -0,0 +1,62 @@
+// projects.js
+// -----------------------------
+// SMGT 490 Projects Data
+// -----------------------------
+
+const PROJECTS = [
+  {
+    id: "p1",
+    year: 2025,
+    title: "Title A",
+    authors: "A",
+    venue: "Sports analytics",
+    summary:
+      "Project A",
+    abstract:
+      "Project A studies ...",
+    links: {
+      doi: "#",
+      code: "https://github.com",
+      poster: null,
+      pdf: null
+    },
+    thumbnail: "assets/project1.png"
+  },
+
+  {
+    id: "p2",
+    year: 2025,
+    title: "Title B",
+    authors: "B",
+    venue: "Sports analytics",
+    summary:
+      "Project B",
+    abstract:
+      "Project B studies ...",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: "assets/project2.png"
+  },
+
+  {
+    id: "p3",
+    year: 2024,
+    title: "Title C",
+    authors: "C",
+    venue: "SMGT 490 Project (2024)",
+    summary:
+      "Title C",
+    abstract:
+      "Title C studies ...",
+    links: {
+      code: "#",
+      poster: "#",
+      pdf: "#"
+    },
+    thumbnail: null
+  }
+];
