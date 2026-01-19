876189bb687173d865700e52f5116bb411451e3e
Hua Gong <hg37@rice.edu>
2026-01-19 15:36:06 -0600

    new data

--- projects.js
+++ projects.js
@@ -6,44 +6,313 @@
 const PROJECTS = [
   {
     id: "p1",
-    year: 2025,
-    title: "Title A",
-    authors: "A",
-    venue: "Sports analytics",
+    year: 2026,
+    title: "What is the relationship between swing path adjustability, measured by variability in swing path tilt, and offensive streakiness among MLB hitters in the 2024 and 2025 seasons?",
+    authors: "Nathaniel Ascher",
+    venue: "Baseball",
     summary:
-      "Project A description",
+      "",
     abstract:
-      "Project A studies ...",
+      "",
     links: {
       doi: "#",
       code: "https://github.com",
       poster: null,
       pdf: null
     },
-    thumbnail: "assets/project1.png"
+    thumbnail: null
   },
 
   {
     id: "p2",
-    year: 2025,
-    title: "Title B",
-    authors: "B",
-    venue: "Sports analytics",
+    year: 2026,
+    title: "New Arsenal-augmented Stuff+ metric",
+    authors: "Theo Au-Yeung",
+    venue: "Baseball",
     summary:
-      "Project B description",
+      "",
     abstract:
-      "Project B studies ...",
+      "",
     links: {
       doi: "#",
       code: 'https://github.com',
       poster: null,
       pdf: null
     },
-    thumbnail: "assets/project2.png"
+    thumbnail: null
   },
-
   {
     id: "p3",
+    year: 2026,
+    title: "Are offenses making a strategic mistake by not utilizing a fullback in their offense, and how can analytics support or not support the value of the fullback?",
+    authors: "Micah Barnett",
+    venue: "Football",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p4",
+    year: 2026,
+    title: "What impact does a pool have on a swimmer's performance?",
+    authors: "Lily Cramer",
+    venue: "Swimming",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p5",
+    year: 2026,
+    title: "How to best predict success for college football players who enter the transfer portal?",
+    authors: "Lucca Ferraz",
+    venue: "Football",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p6",
+    year: 2026,
+    title: "Using Markov Chain Decision Process to evaluate batter and bowler (pitcher) performance in T20 Cricket",
+    authors: "Kushal Gupta",
+    venue: "Cricket",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p7",
+    year: 2026,
+    title: "Quantifying Coaching Value in the National Football League",
+    authors: "Will Kinnebrew",
+    venue: "Football",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p8",
+    year: 2026,
+    title: "Should players be changing their attack angle based on situation/at-bat specific goals or focus on a repeatable, constant swing regardless of circumstances?",
+    authors: "Ian Kleppinger",
+    venue: "Baseball",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p9",
+    year: 2026,
+    title: "How pitch sequencing affect batter swing quality",
+    authors: "Dante Maurice",
+    venue: "Baseball",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p10",
+    year: 2026,
+    title: "Are teams that win by large margins in one game less likely to cover the betting spread in their next game, after controlling for underlying team strength and contextual factors?",
+    authors: "Sean McGarry",
+    venue: "Sports Betting",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p11",
+    year: 2026,
+    title: "For quarterbacks who are already established in the league ('mid-career'), can similarity-based forecasts outperform standard baselines?",
+    authors: "Elisabeth Millington",
+    venue: "Football",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p12",
+    year: 2026,
+    title: "Can NASCAR drivers be clustered into distinct in-race behavioral archetypes (e.g., 'Aggressive Chargers','Tire Conservers') based on strategic decisions, and do these styles predict success on different track types?",
+    authors: "Savan Patel",
+    venue: "NASCAR",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p13",
+    year: 2026,
+    title: "Are pitchers making the optimal pitch selection based on their pitch quality?",
+    authors: "Andersen Pickard",
+    venue: "Baseball",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p14",
+    year: 2026,
+    title: "How do player skill traits interact with the extreme run environment of Coors Field, and which types of hitters and pitchers are best suited to perform well there compared to league-average parks?",
+    authors: "Kenneth Soh",
+    venue: "Baseball",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p15",
+    year: 2026,
+    title: "Optimizing save decisions in Counter-Strike 2",
+    authors: "Lou Zhou",
+    venue: "CS2",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+   {
+    id: "p16",
+    year: 2026,
+    title: "How do defensive personnel groupings and sub-package strategies affect overall defense performance in the NFL, and which approaches deliver the greatest performance per dollar of salary investment?",
+    authors: "Peyton Stevenson",
+    venue: "Football",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+     {
+    id: "p17",
+    year: 2026,
+    title: "",
+    authors: "Alicia Garza",
+    venue: "",
+    summary:
+      "",
+    abstract:
+      "",
+    links: {
+      doi: "#",
+      code: 'https://github.com',
+      poster: null,
+      pdf: null
+    },
+    thumbnail: null
+  },
+  {
+    id: "p100",
     year: 2024,
     title: "Title C",
     authors: "C",
@@ -57,6 +326,6 @@ const PROJECTS = [
       poster: "#",
       pdf: "#"
     },
-    thumbnail: null
+    thumbnail: "assets/project1.png"
   }
 ];
